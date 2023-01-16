fd1b9ccc69387b0e564d87ddb010aad4262af04a
TechShark20 <122713809+TechShark20@users.noreply.github.com>
2023-01-16 18:23:39 +0530

    Add files via upload

---
+++ stock.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 15 18:54:10 2023
+
+@author: Samar jain
+
+"""
+import matplotlib.pyplot as plt
+import datetime
+from re import L
+import pandas as pd
+import streamlit as st 
+import os 
+import glob 
+os.chdir(r"C:\Users\hp\Desktop\datascience\stockprice\archive")
+all_files= glob.glob(r"C:/Users/hp/Desktop/datascience/stockprice/archive/*csv")
+df = pd.concat((pd.read_csv(f) for f in all_files))
+st.title('Stock market analyser')
+st.write("""
+Analysis of Nifty 50 stocks
+""")
+df.drop(columns=['Trades','Series'])
+shname='TATASTEEL'
+st.write("""
+enter particular details
+
+""")
+tab1, tab2, tab3 = st.tabs(["Stockname", "Startdate", "Enddate"])
+with tab1:
+  shname=st.text_input("enter the name of your share ")
+shname=shname.upper()
+df1=df[df['Symbol']==shname]
+with tab2:
+  d1 = st.date_input( "Enter start date to view",datetime.datetime(2011, 1, 1))
+with tab3:
+  d2 = st.date_input( "Enter start date to view",datetime.datetime(2011, 7, 1))
+df1['Date']= pd.to_datetime(df1['Date'])
+df2 = df1[(df1['Date']>pd.Timestamp(d1))&(df1['Date']<pd.Timestamp(d2))]
+st.write("showing the chart for"+shname)
+st.dataframe(df2)
+st.write("""closing price chart""")
+chart1,chart2=st.tabs(['Closing','Volume'])
+with chart1:
+  st.header('Closing price chart')
+  st.line_chart(df2,x='Date',y=['Close'])
+with chart2:
+  st.header(' volumes traded')
+  st.line_chart(df2,x='Date',y=['Volume'])
